a76aef17a330c0757dfe2179c9396332c27fe337
friestha <friesthaa@gmail.com>
2023-09-19 19:19:15 +0800

    Menambahkan praktek array, objek, perulangan, dan fungsi

--- iodash.js
+++ iodash.js
@@ -1,5 +1,33 @@
+// main.js
 const _ = require('lodash');
-const numbers = [1, 2, 3, 4, 5];
-const sum = _.sum(numbers);
 
-console.log(`Hasil penjumlahan: ${sum}`);
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = _.map(numbers, (num) => num * 2);
+
+console.log(doubledNumbers);
+
+// Menggunakan array
+const fruits = ['apple', 'banana', 'cherry'];
+console.log(fruits.length); // Output: 3
+console.log(fruits[0]); // Output: 'apple'
+
+// Menggunakan objek
+const person = {
+    name: 'frista',
+    age: 20,
+    job: 'mahasiswa'
+};
+
+console.log(person.name); // Output: 'frista'
+
+// Perulangan for
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  
+  // Fungsi
+  function greet(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  greet('frista'); // output: Hello, frista!
+  
